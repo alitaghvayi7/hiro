e2149141eca0e268238e77e87ca2687cf955fc4e
alitaghvayi7 <alitaghvaei78@gmail.com>
2024-05-29 00:32:58 +0330

    advertise

---
+++ src/app/(mainsite)/advertise/page.tsx
@@ -0,0 +1,13 @@
+export default function AdvertisePage (){
+    return(
+        <>
+            <section className="w-screen overflow-hidden overflow-y-auto bg-gradient-to-b from-[#024343] to-[#001212] ">
+                  <span className={`text-white font-poppins font-medium leading-6 text-[16px] cursor-pointer flex w-full justify-end`}>
+                        EN  /  FA
+                  </span>
+
+            </section>
+        </>
+
+    )
+}
