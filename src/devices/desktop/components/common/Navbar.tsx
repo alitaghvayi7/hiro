d402e43d3fce31a751577f5b178c35df8c0a916c
Smok455 <46872648+Smok455@users.noreply.github.com>
2024-06-04 18:35:42 +0330

    header desktop new design

--- src/devices/desktop/components/common/Navbar.tsx
+++ src/devices/desktop/components/common/Navbar.tsx
@@ -2,10 +2,11 @@ import { Button, buttonVariants } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 import { ArrowDownTrayIcon } from "@heroicons/react/24/outline"
 import Image from "next/image"
-import {Link} from "@/navigation"
+import { Link } from "@/navigation"
 import Logo from "@/assets/images/logo-lg.png"
+import HirooTextLogo from '@/assets/images/hiroo-text-logo.svg';
 import Menu from "./Menu"
-import {useTranslate} from "@/lib/hooks/useTranslate";
+import { useTranslate } from "@/lib/hooks/useTranslate";
 import LanguageDropdown from "@/devices/desktop/components/common/LanguageDropdown";
 
 
@@ -17,12 +18,13 @@ const Navbar = () => {
 
             <nav
                 className={` w-full h-[60px] bg-transparent flex mt-8 items-center gap-[294px] justify-between relative z-[-10] isolate`}>
-                <div className={`flex items-center gap-[94px]`}>
+                <div className={`flex items-center gap-4 justify-center`}>
                     <Image src={Logo} alt={''} className={`h-[60px] w-[40px] drop-shadow-sm object-cover`} />
-                    <Menu />
+                    <Image src={HirooTextLogo} alt={''} className={`h-[18px] w-[60px] drop-shadow-sm object-cover`} />
                 </div>
+                <Menu />
                 <div className={`flex items-center gap-10`}>
-                    <LanguageDropdown/>
+                    <LanguageDropdown />
                     <Button
                         variant={'outline'}
                         className={`border-yellow-100 text-yellow-100 w-[154px] rounded-[8px] h-[40px] gap-1 flex font-poppins text-base font-light`}
